aadac2efc66b5e13be48eadf5709c127a006cb41
SOUS021 <joao.vale@arvato.com>
2020-09-14 22:38:38 +0300

    safe producer

--- src/main/java/com/github/learn/kafka/twitter/TwitterProducer.java
+++ src/main/java/com/github/learn/kafka/twitter/TwitterProducer.java
@@ -97,6 +97,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        //create safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         //create the producer
         return new KafkaProducer<>(properties);
 
