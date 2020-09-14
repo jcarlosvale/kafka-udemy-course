b30a12d953c2af193504147189837d0040b64f0b
SOUS021 <joao.vale@arvato.com>
2020-09-14 11:39:57 +0300

    consumer example

--- src/main/java/com/github/learn/kafka/ConsumerDemo.java
+++ src/main/java/com/github/learn/kafka/ConsumerDemo.java
@@ -1,4 +1,44 @@
 package com.github.learn.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+@Slf4j
 public class ConsumerDemo {
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "first_topic";
+
+        //create consumer properties
+        Properties properties= new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        //subscribe consumer to our topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+        //poll for new data
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record: records) {
+                log.info("Key: {}, Value: {}", record.key(), record.value());
+                log.info("Partition: {}, Offset: {}", record.partition(), record.offset());
+            }
+        }
+    }
 }
